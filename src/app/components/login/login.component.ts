7ed9125ab354d47f2c6f136474aa5b8174214862
JulioBarrios <juliobarrios0296@gmail.com>
2022-09-19 23:00:38 -0500

    chore: Added a Property Binding on the password input and an Event Binding (click) on the password field

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  passwordType: string = 'password';
+  passwordShown: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public togglePassword(): void{
+    if(this.passwordShown){
+      this.passwordShown = false;
+      this.passwordType = 'password';
+    }else{
+      this.passwordShown = true;
+      this.passwordType = 'text';
+    }
+  }
 }
