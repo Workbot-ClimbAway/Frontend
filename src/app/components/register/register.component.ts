329077e6fd210dfe2cbfcc8ed77d6d1f40c125b1
JulioBarrios <juliobarrios0296@gmail.com>
2022-09-20 14:28:04 -0500

    chore: added angular reactive form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-register',
@@ -6,21 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm !: FormGroup;
   passwordType: string = 'text';
   passwordShown: boolean = true;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required,
+        Validators.pattern(/^([a-z]+)([A-Z]+)([0-9]+)|([A-Z]+)([a-z]+)([0-9]+)|([A-Z]+)([0-9]+)([a-z]+)|([a-z]+)([0-9]+)([A-Z]+)|([0-9]+)([a-z]+)([A-Z]+)|([0-9]+)([A-Z]+)([a-z]+)$/),
+        Validators.minLength(5)]),
+      terms: new FormControl(false, [Validators.requiredTrue])
+    })
+  }
 
   ngOnInit(): void {
+
   }
 
   togglePassword(){
     if(this.passwordShown){
+      this.passwordType = 'password';
       this.passwordShown = false;
     }else{
       this.passwordType = 'text';
       this.passwordShown = true;
     }
+  }
+
+  sendForm(){
+    console.log(this.registerForm)
+  }
+
+  openDialog(){
+    //.dialog.open(DialogConfirmComponent);
   }
 
 }
